be138f5b3ac9d0acc7f303e98c9185cbc13d7a3d
pinal882 <pinal.shah882@gmail.com>
2023-11-14 23:37:12 +0000

    my homework

---
+++ src/Project_2_Com_Orangehrmlive.java
@@ -0,0 +1,76 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Project_2_Com_Orangehrmlive {
+
+
+    static String baseUrl = "https://opensource-demo.orangehrmlive.com/";
+    static String browser ="Chrome";
+    public static void main(String[] args) throws InterruptedException {
+
+        // 1. Setup Chrome browser
+        WebDriver driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver" , "chromedriver.exe");
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        driver.manage().window().maximize();
+        //WebDriver driver = new FirefoxDriver();
+        //WebDriver driver = new EdgeDriver();
+        //WebDriver driver = new SafariDriver();
+
+        //2. Open URL
+        driver.get(baseUrl);
+
+        //3. Print the title of the page
+        String title = driver.getTitle();
+        System.out.println("Title of the Page :" +title);
+
+        // 4. Print the current url
+        System.out.println("The current Url: "+ driver.getCurrentUrl());
+
+        //5. Print the page source
+        System.out.println(driver.getPageSource());
+        Thread.sleep(5000);
+
+        //6. Click on ‘Forgot your password?’ link.
+        WebElement clickLink = driver.findElement(By.className("orangehrm-login-forgot"));
+        clickLink.click();
+        Thread.sleep(3000);
+
+        //7. Print the current url
+        System.out.println("The current Url: "+ driver.getCurrentUrl());
+
+        //8. Navigate back to the login page
+        driver.navigate().back();
+        Thread.sleep(3000);
+
+        //9. Refresh the page.
+        driver.navigate().refresh();
+
+        //10. Enter the email to email field.
+        WebElement userName = driver.findElement(By.name("username"));
+        userName.sendKeys("Admin");
+
+        //11. Enter the password to password field
+        WebElement password = driver.findElement(By.name("password"));
+        password.sendKeys("admin123");
+        Thread.sleep(3000);
+
+        //12. Click on Login Button.
+        WebElement loginClick = driver.findElement(By.xpath("(//button[@type='submit'])"));
+        loginClick.click();
+        Thread.sleep(5000);
+
+        //13. Close the browser
+        driver.quit();
+
+
+
+
+
+
+}
+}
